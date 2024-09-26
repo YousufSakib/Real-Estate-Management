7cc3387b4a8c16e9ee6afa521d625602981ebf56
yousufSakib <yousufsakib29603@gmail.com>
2024-09-26 22:58:36 +0600

    backend complete

--- src/routes/profileUpdatePage/profileUpdatePage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function profileUpdatePage() {
-  return <div>profileUpdatePage</div>;
-}
-
-export default profileUpdatePage;
--- src/routes/singlePage/singlePage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function singlePage() {
-  return <div>singlePage</div>;
-}
-
-export default singlePage;
